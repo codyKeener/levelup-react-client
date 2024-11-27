7152bdf18d185d35c077f1ba3c602673cb69ca5e
Cody Keener <codykeener@Codys-MBP.localdomain>
2024-11-26 20:32:29 -0600

    Created GameForm and EventForm

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import { Button } from 'react-bootstrap';
 import EventCard from '../../components/event/EventCard';
 import { getEvents } from '../../utils/data/eventData';
 
 function Home() {
   const [events, setEvents] = useState([]);
+  const router = useRouter();
 
   useEffect(() => {
     getEvents().then((data) => setEvents(data));
@@ -11,7 +14,14 @@ function Home() {
 
   return (
     <article className="events">
-      <h1>Events</h1>
+      <div style={{ display: 'flex', justifyContent: 'space-between', margin: '10px 0 10px' }}>
+        <h1>Events</h1>
+        <Button onClick={() => {
+          router.push('/events/new');
+        }}
+        >Register New Event
+        </Button>
+      </div>
       {events.map((event) => (
         <section key={`event--${event.id}`} className="event">
           <EventCard game={event.game} description={event.description} date={event.date} time={event.time} organizer={event.organizer} />
